d033e50c474231b063c737fec5889968b8bdb97e
Palana <palana@stunned.de>
2014-10-26 02:03:13 +0200

    Split on_streamButton_clicked into StartStreaming/StopStreaming

--- obs/window-basic-main.hpp
+++ obs/window-basic-main.hpp
@@ -150,6 +150,9 @@ private:
 	void OpenProjector(obs_source_t *source, int monitor);
 
 public slots:
+	void StartStreaming();
+	void StopStreaming();
+
 	void StreamingStart();
 	void StreamingStop(int errorcode);
 
