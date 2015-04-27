421f74556dab86536ebf9c51a2e5a3688509337b
jp9000 <obs.jim@gmail.com>
2015-04-27 02:11:04 -0700

    Flip mac control/command key strings

--- libobs/obs-cocoa.c
+++ libobs/obs-cocoa.c
@@ -1104,7 +1104,7 @@ void obs_key_combination_to_str(obs_key_combination_t combination,
 		struct dstr *str)
 {
 	if ((combination.modifiers & INTERACT_CONTROL_KEY) != 0) {
-		add_combo_key(OBS_KEY_META, str);
+		add_combo_key(OBS_KEY_CONTROL, str);
 	}
 	if ((combination.modifiers & INTERACT_ALT_KEY) != 0) {
 		add_combo_key(OBS_KEY_ALT, str);
@@ -1113,7 +1113,7 @@ void obs_key_combination_to_str(obs_key_combination_t combination,
 		add_combo_key(OBS_KEY_SHIFT, str);
 	}
 	if ((combination.modifiers & INTERACT_COMMAND_KEY) != 0) {
-		add_combo_key(OBS_KEY_CONTROL, str);
+		add_combo_key(OBS_KEY_META, str);
 	}
 	if (combination.key != OBS_KEY_NONE) {
 		add_combo_key(combination.key, str);
