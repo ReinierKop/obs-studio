757a2a87cd08bb1d4cea0b7573db54c16a819c8d
kop-reinier <kop.reinier@gmail.com>
2015-04-30 15:20:10 +0200

    reduce logged messages and removed GUI to prepare for headless ubuntu inclusion

--- libobs/util/base.c
+++ libobs/util/base.c
@@ -20,11 +20,13 @@
 #include "c99defs.h"
 #include "base.h"
 
-#ifdef _DEBUG
+/*#ifdef _DEBUG
 static int log_output_level = LOG_DEBUG;
 #else
 static int log_output_level = LOG_INFO;
-#endif
+#endif*/
+
+static int log_output_level = LOG_WARNING;
 
 static int  crashing     = 0;
 static void *log_param   = NULL;
